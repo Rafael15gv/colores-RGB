965a3b96ee301496be16f0b83fcc3edca1caf138
Rafael15gv <143367099+Rafael15gv@users.noreply.github.com.>
2024-10-23 10:28:53 -0500

    depuracion archivo JavaScript menos lineas de codigo

--- index.js
+++ index.js
@@ -1,27 +1,17 @@
-const barR = document.getElementsByTagName('input')[0];
-const barG = document.getElementsByTagName('input')[1];
-const barB = document.getElementsByTagName('input')[2];
-const red = document.getElementById('red');
-const green = document.getElementById('green');
-const blue = document.getElementById('blue');
-const fondo = document.getElementsByTagName('body')[0];
+const barR = document.getElementById('barraRoja');
+const barG = document.getElementById('barraVerde');
+const barB = document.getElementById('barraAzul');
+const textoRed = document.getElementById('red');
+const textoGreen = document.getElementById('green');
+const textoBlue = document.getElementById('blue');
 
-const barras =['barR','barG','barB'];
-const textos =['red','green','blue'];
-    barR.addEventListener('click', () => {
-        console.log(barR.value);
-        red.innerText = barR.value;
-        fondo.style.background = `rgb(${barR.value},${barG.value},${barB.value})`;
-        
-    })
-    barG.addEventListener('click', () => {
-        console.log(barG.value);
-        green.innerText = barG.value;
-        fondo.style.background = `rgb(${barR.value},${barG.value},${barB.value})`;
-    })
-    barB.addEventListener('click', () => {
-        console.log(barB.value);
-        blue.innerText = barB.value;
-        fondo.style.background = `rgb(${barR.value},${barG.value},${barB.value})`;
-    })
-    
+    barR.addEventListener('change', () => {color();})
+    barG.addEventListener('change', () => {color();})
+    barB.addEventListener('change', () => {color();})
+
+function color() {
+  textoRed.innerText = barR.value;
+  textoGreen.innerText = barG.value;
+  textoBlue.innerText = barB.value;
+  document.body.style.background = `rgb(${barR.value},${barG.value},${barB.value})`;
+}
